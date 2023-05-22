00cf3126c42963bfe8a455f630728422ed2205a0
thanapoom21 <thanapoom.p.music@gmail.com>
2023-05-22 11:34:44 -0700

    build a landing page

--- app/mbuttons/src/pages/index.tsx
+++ app/mbuttons/src/pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import { FiCloud, FiCode } from 'react-icons/fi'
+import Code from '../components/Code'
+import Footer from '../components/Footer'
+import Common from './Common'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
@@ -14,110 +17,54 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <div className="home">
+        <Common activePage={''} showMenuButton={false} />
+        <Hero />
+        <div className="content container px-4 mx-auto">
+          <section className="p-4">
+            <h2 className="text-3xl font-bold text-black-600">
+              <span className="inline-block mr-3 align-middle">
+                <FiCode size="2rem" />
+              </span>
+              Installation
+            </h2>
+            <div className="gap-12 grid md:grid-cols-2 text-xl mt-2">
+              <p>
+                Install sButtons’s source LESS via npm.
+                Package managed installs don’t include documentation or our full build scripts.
+                You can also use our npm.
+              </p>
+              <div>
+                <Code code={`npm install sbuttons`} language="shell" />
+              </div>
+            </div>
+          </section>
+          <section className="p-4">
+            <h2 className="text-3xl font-bold text-black-600">
+              <span className="inline-block mr-3 align-middle">
+                <FiCloud size="2rem" />
+              </span>
+              sButtons CDN
+            </h2>
+            <div className="gap-12 grid md:grid-cols-2 grid-cols-1 text-xl mt-2">
+              <p>
+                When you only need to include sButtons’ compiled CSS , you can use
+                <a href="https://cdn.jsdelivr.net/npm/sbuttons/dist/sbuttons.min.css"
+                  target="_blank"
+                  className="text-gray-600 dark:text-white underline-red ml-1"
+                >
+                  sButtons CDN
+                </a>.
+              </p>
+              <div>
+                <Code code={`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sbuttons/dist/sbuttons.min.css">`}
+                  language="html" />
+              </div>
+            </div>
+          </section>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <Footer />
+      </div>
     </>
   )
 }
