c8a30f076d0afbab5d90fc3929522c28eb323238
thanapoom21 <thanapoom.p.music@gmail.com>
2023-05-26 10:38:07 -0700

    add section header and link for documentation page

---
+++ app/mbuttons/src/components/SectionLink.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FiHash } from 'react-icons/fi'
+
+function SectionLink({ to }: { to: string }) {
+  return (
+    <a
+      href={`#${to}`}
+      className="hover:opacity-100 inline-block mr-2 opacity-80 text-red-600"
+    >
+      <FiHash className="inline" />
+    </a>
+  )
+}
+
+export default SectionLink
